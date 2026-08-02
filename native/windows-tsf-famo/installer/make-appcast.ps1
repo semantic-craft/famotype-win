77d74bc036dd4c4ec7f393734673e93fd3338dad
semantic-craft <14216877+semantic-craft@users.noreply.github.com>
2026-08-02 18:15:48 +0800

    chore(release): prepare 1.5.15

--- native/windows-tsf-famo/installer/make-appcast.ps1
+++ native/windows-tsf-famo/installer/make-appcast.ps1
@@ -7,7 +7,7 @@
 #>
 param(
   [ValidatePattern('^\d+\.\d+\.\d+$')]
-  [string] $AppVersion = '1.5.14',
+  [string] $AppVersion = '1.5.15',
   [string] $InstallerPath = '',
   [string] $PrivateKeyPath = $env:FAMO_UPDATE_PRIVATE_KEY,
   [string] $OutputPath = ''
