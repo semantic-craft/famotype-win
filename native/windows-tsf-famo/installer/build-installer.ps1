46c44bba46c19eb5a1e080b0171229144256a1f0
semantic-craft <14216877+semantic-craft@users.noreply.github.com>
2026-08-03 14:16:01 +0800

    chore(release): prepare 1.5.19

--- native/windows-tsf-famo/installer/build-installer.ps1
+++ native/windows-tsf-famo/installer/build-installer.ps1
@@ -10,7 +10,7 @@ param(
   [string] $NativeOutput = '',
   [string] $BridgeArtifact = '',
   [ValidatePattern('^\d+\.\d+\.\d+$')]
-  [string] $AppVersion = '1.5.18',
+  [string] $AppVersion = '1.5.19',
   [ValidateSet('Stable')]
   [string] $Identity = 'Stable',
   [string] $Configuration = 'Release',
